ec20e9bc14db81612c59677def4dfeaada1653ad
Airat G <darkairx@mail.ru>
2022-08-04 18:47:07 +0300

    добавил задачи семинар

--- 032/Program.cs
+++ 032/Program.cs
@@ -1,14 +1,14 @@
 ﻿//Задать массив из 8 целых элементов и вывести их на экран
-int N=8;
-int[] a=new int[N];
-Random random=new Random();//псевдослучайные числа
-for(int i=0;i<a.Length;i++)
-    a[i]=random.Next(0,11);
+int N = 8;
+int[] a = new int[N];
+Random random = new Random();//псевдослучайные числа
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(0, 11);
 
 
 
 
-for(int i=0;i<a.Length;i++)
+for (int i = 0; i < a.Length; i++)
     System.Console.Write($"{a[i],4}");
 
 
