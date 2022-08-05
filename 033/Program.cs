bf3972f88b9bd47ac00607149e0ee81be95761a5
Airat G <darkairx@mail.ru>
2022-08-05 16:15:01 +0300

    добавил задачи семинар

--- 033/Program.cs
+++ 033/Program.cs
@@ -1,24 +1,12 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 int N = 8;
-int min = 0;
-int max = 1;
+int[] a = new int[N];
+Random random = new Random();
+for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(0, 2);
 
-int[] CreateArray(int N, int min, int max)
-{
-    int[] arr = new int[N];
-    for (int i = 0; i < N; i++)
-    {
-        arr[i] = new Random().Next(min, max + 1);
-    }
-    return arr;
-}
 
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        System.Console.Write($"{array[i]}");
-    }
-}
 
-PrintArray(CreateArray(N, min, max));
+
+for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");
