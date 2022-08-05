1c9169b5c75f893c888d73bc79c19dbac0ed900f
Airat G <darkairx@mail.ru>
2022-08-05 17:26:39 +0300

    добавил задачи семинар

---
+++ 035/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу замены элементов массива на противоположные
+int N = 8;
+int[] a = new int[N];
+Random random = new Random();
+
+    for (int i = 0; i < a.Length; i++)
+    a[i] = random.Next(0, 9);
+    for (int i = 0; i < a.Length; i++)
+    a[i] = a[i]*-1;
+
+
+    for (int i = 0; i < a.Length; i++)
+    System.Console.Write($"{a[i],4}");
