e6c9da414c606caa16294eeb8e8276efbee5d948
Airat.G <darkairx@mail.ru>
2022-08-17 20:34:21 +0300

    Обновлен

---
+++ 043/Program.cs
@@ -0,0 +1,15 @@
+﻿// С клавиатуры вводится число N. Затем вводятся N чисел. Определить сколько чисел больше 0 введено с клавиатуры
+
+
+int numbers = 5;
+int count = 0;
+
+while (numbers > 0)
+{
+    System.Console.WriteLine("Введите число: ");
+    string num = Console.ReadLine();
+    int number = Convert.ToInt32(num);
+    if(number > 0) count++;
+    numbers--;
+}
+System.Console.WriteLine($"Количество чисел больше 0 = {count}");
---
+++ 046/Program.cs
@@ -0,0 +1,13 @@
+﻿// С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. Принять первые числа равными 0 и 1
+int N = Convert.ToInt32(Console.ReadLine());
+int Fibonacci(int n)
+{
+    if(n==1 || n==2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+
+}
+
+for (int i = 1; i < N; i++)
+{
+    System.Console.WriteLine(Fibonacci(i));
+}
