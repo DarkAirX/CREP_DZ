4d0dd2787832ddf789f0c5c64434a41e6654c66c
Airat.G <darkairx@mail.ru>
2022-08-25 10:39:48 +0300

    Save

---
+++ 036A/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+
+ int c = 0;
+            Random Rand = new Random();
+            int[] a = new int[4];
+            Console.WriteLine("");
+
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Rand.Next(0, 100);
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine("");
+int result = 0;
+ for (int i = 0; i < a.Length; i++)
+ 
+
+            {
+                if (a[i] % 2 != 0)
+                {
+                    result += a[i];
+                }
+              
+            }
+            Console.WriteLine(result);
---
+++ 34(A)/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+  int c = 0;
+            Random Rand = new Random();
+            int[] a = new int[4];
+            Console.WriteLine("");
+
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = Rand.Next(100, 300);
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine("");
+
+ for (int i = 0; i < a.Length; i++)
+ 
+
+            {
+                if ((a[i] % 10) % 2 == 0)
+                {
+                    c++;
+                }
+              
+            }
+            Console.WriteLine("Количество четных цифр: {0}", c);
+                        
+               
+        
+      
