d0f6c9505191b65dc178c3d1b570f57ad8b3a98f
Airat.G <darkairx@mail.ru>
2022-09-16 22:05:07 +0300

    update

---
+++ recursion/Program.cs
@@ -0,0 +1,8 @@
+﻿// написать программу вывода чисел от 1 до N без использования циклов и оператора
+
+void loop (int i,int N)
+{
+    System.Console.WriteLine(i);
+    if(i<N) loop(i+1,N);//рекурсия
+}
+loop(1,10);
---
+++ string5/Program.cs
@@ -0,0 +1,15 @@
+﻿// 5. Определить являются ли введенные с клавиатуры символы правильной записью целого числа.
+// Вычислить сумму цифр введенного числа
+
+
+string num = Console.ReadLine();
+
+            int temp = 0;
+            if (!Int32.TryParse(num, out temp))
+                Console.WriteLine("Это не является верной записью целого числа");
+            else {
+                int sum = 0;
+                foreach (char digit in num)
+                    sum += digit - 48;
+                Console.WriteLine("Сумма цифр = " + sum);
+            }
